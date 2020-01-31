823285d35d6f522c55ec2f0ea397da182d224918
Hilko Janssen <hilkojj@outlook.com>
2020-01-31 17:01:51 +0100

    Update initialize_project.py

--- initialize_project.py
+++ initialize_project.py
@@ -23,9 +23,10 @@ def file(url, dst):
 
 # command("git init")
 command(f"git submodule add {GU_REPO} gu")
+command("git submodule update --init --recursive")
 
-command("pip install --user glad")
-command("glad --generator=c --extensions=GL_EXT_texture_compression_s3tc --out-path=gu/external/glad")
+command("python -m pip install --user glad")
+command("python -m glad --generator=c --extensions=GL_EXT_texture_compression_s3tc --out-path=gu/external/glad")
 
 zip("https://github.com/glfw/glfw/releases/download/3.2.1/glfw-3.2.1.zip", "gu/external")
 zip("https://github.com/g-truc/glm/releases/download/0.9.9.5/glm-0.9.9.5.zip", "gu/external/glm-0.9.9.5")
